62d9f84548b165a3ff3f5b49ab3ac7f737bf3c00
unknown <alumnos@.upc.edu.pe>
2012-10-10 20:06:17 -0500

    nuevo proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PRESTAMO::Application.config.secret_token = '78fe0f44b85cb2c3d4eabd5ff1190d67c73841511e7e79284a4517e24090afe38c438d0c45f19f7a0a678971780bc07b03f27785183a06300fc348eebc52498b'
